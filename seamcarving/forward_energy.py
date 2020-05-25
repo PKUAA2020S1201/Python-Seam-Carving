779fe652f321cdb7aa275e2c1f193c23c2b600d9
yousiki <you.siki@outlook.com>
2020-05-25 15:48:59 +0800

    update forward energy

--- seamcarving/forward_energy.py
+++ seamcarving/forward_energy.py
@@ -4,32 +4,39 @@ import cv2
 
 
 @nb.jit
-def forward_energy(image):
-    h, w, c = image.shape
-    m = np.zeros((h, w))
-    energy = np.zeros((h, w))
-    I = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    
-    for i in range(1, h):
-        for j in range(w):
-            up = (i - 1) % h
-            down = (i + 1) % h
-            left = (j - 1) % w
-            right = (j + 1) % w
-            
-            cU = np.abs(I[i, right] - I[i, left])
-            cL = np.abs(I[up, j] - I[i, left]) + cU
-            cR = np.abs(I[up, j] - I[i, right]) + cU
-            
-            mU = m[up, j]
-            mL = m[up, left]
-            mR = m[up, right]
-            
-            cULR = np.array([cU, cL, cR])
-            mULR = np.array([mU, mL, mR]) + cULR
-            
-            x = np.argmin(mULR)
-            m[i, j] = mULR[x]
-            energy[i, j] = cULR[x]
-    
-    return energy
+def distance(x, y):
+    return np.sqrt(np.sum(np.square(x - y)))
+
+
+@nb.jit
+def seam_with_minimum_forward_energy(image):
+    n, m, c = image.shape
+    f = np.zeros((n, m), dtype=np.float32)
+    g = np.zeros((n, m), dtype=np.int)
+    for j in range(1, m - 1):
+        if j == 0:
+            delta = distance(image[0, j + 1], image[0, j]) * 2
+        elif j == m - 1:
+            delta = distance(image[0, j - 1], image[0, j]) * 2
+        else:
+            delta = distance(image[0, j - 1], image[0, j + 1])
+        f[0, j] = delta
+    for i in range(1, n):
+        for j in range(m):
+            tmp = np.zeros(3, dtype=np.float32)
+            tmp[1] = f[i - 1, j]
+            tmp[0] = np.inf if j == 0 else f[i - 1, j - 1] + np.sum(np.abs(image[i, j - 1] - image[i - 1, j]))
+            tmp[2] = np.inf if j == m - 1 else f[i - 1, j + 1] + np.sum(np.abs(image[i, j + 1] - image[i - 1, j]))
+            if j == 0:
+                delta = distance(image[i, j + 1], image[i, j]) * 2
+            elif j == m - 1:
+                delta = distance(image[i, j - 1], image[i, j]) * 2
+            else:
+                delta = distance(image[i, j - 1], image[i, j + 1])
+            g[i, j] = j - 1 + np.argmin(tmp)
+            f[i, j] = np.min(tmp) + delta
+    seam = np.zeros(n, dtype=np.int)
+    j = np.argmin(f[n - 1])
+    for i in range(n - 1, 0, -1):
+        seam[i], j = j, g[i, j]
+    return seam
