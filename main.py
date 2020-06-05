38b5b451eaba857f4f798bcc70568fed85ef7a80
yousiki <you.siki@outlook.com>
2020-06-05 10:04:03 +0800

    upd

--- main.py
+++
@@ -1,7 +0,0 @@
-import sc2
-
-
-if __name__ == "__main__":
-    image = sc2.utils.image_load("jinge")
-    gradient = sc2.energy.entropy_energy(image)
-    sc2.utils.image_show(gradient, freeze=True)
